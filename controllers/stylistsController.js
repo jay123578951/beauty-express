84332f7120fc9ed03b117c3f15e943933d7575de
jay <jay@ocard.co>
2025-01-01 21:54:42 +0800

    feat: add cors setting in test & stylists api

--- controllers/stylistsController.js
+++ controllers/stylistsController.js
@@ -2,19 +2,30 @@ const connection = require("../database");
 
 // 取得設計師 API
 exports.getStylists = (req, res) => {
-  connection.query(
-    "SELECT id, name, photo, email, phone, specialty FROM stylists",
-    (err, results) => {
-      if (err) {
-        console.error("Database error:", err);
-        return res.status(500).json({ error: "Database error" });
-      }
+  const storeId = req.query.store_id;
 
-      if (results.length === 0) {
-        return res.status(404).json({ message: "No stylists found" });
-      }
+  if (!storeId) {
+    return res.status(400).json({ error: "store_id is required" }); // 若未提供 store_id，回傳錯誤
+  }
 
-      res.json(results);
+  const query = `
+    SELECT id, name, photo, email, phone, specialty 
+    FROM stylists 
+    WHERE store_id = ?
+  `;
+
+  connection.query(query, [storeId], (err, results) => {
+    if (err) {
+      console.error("Database error:", err);
+      return res.status(500).json({ error: "Database error" });
     }
-  );
+
+    if (results.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No stylists found for the specified store" });
+    }
+
+    res.json(results);
+  });
 };
